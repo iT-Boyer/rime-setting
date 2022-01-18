c61876438a6ae85523ccf61925b46192f8537797
iTBoyer <iTBoyer@qq.com>
2022-01-19 00:49:52 +0800

    添加squirrel支持lua脚本，使用键盘执行脚本的功能，让键盘支持联想

--- rime.lua
+++ rime.lua
@@ -6,3 +6,15 @@ select_character_processor = require("select_character")
 -- 详见 `lua/easy_en.lua`
 local easy_en = require("easy_en")
 easy_en_enhance_filter = easy_en.enhance_filter
+
+-- jimmy54/rime-lua.git
+date_translator = require("date")
+
+module2={
+  {module='command', module_name="cammand_proc",name_space="command" },
+  {module='english', module_name="english_proc",name_space="english" },
+  {module="conjunctive", odule_name = "conjunctive_proc",name_space="conjunctive"},
+  { module= 'multi_reverse', module_name= "multi_reverse__proc", name_space= "multi_reverse" },
+}
+
+init_processor = require('init_processor')
